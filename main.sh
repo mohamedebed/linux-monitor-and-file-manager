4761e064de21f6da65d99ccc19f639bbade86b94
Mohamed Ebed <mohamed9ebed@gmail.com>
2025-10-17 15:07:59 +0300

    project folders

---
+++ main.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+while true; do
+    echo "................................."
+    echo "[ Welcome to the menu ]"
+    echo "[ Choose what you want to make ]"
+    echo "1) File Management"
+    echo "2) System Monitoring"
+    echo "3) Exit"
+    echo "................................."
+    read -p "Choose an option [1-3]: " option
+
+    case $option in
+        1) bash file_manager.sh ;;   
+        2) bash system_monitor.sh ;;
+        3) echo "Goodbye!"; exit 0 ;; 
+        *) echo "Invalid choice!"; sleep 1 ;;  # sleep makes it wait 1s
+    esac
+done
+
